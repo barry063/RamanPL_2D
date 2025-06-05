5cff3ec5166af774d6d00c35c4e3de4d08159262
barry063 <yuhao19980603@gmail.com>
2025-06-05 11:19:16 +0100

    modified:   README.md 	new file:   example-usage/Ramanfit/Raman Sample 532nm 2L-Graphene.txt 	added bi-layer graphene fitting example:   example-usage/Ramanfit/Raman_component.ipynb 	added graphene and bilayer graphene library:   example-usage/Ramanfit/raman_materials.json 	modified the docstring:   src/Mapping.py 	added graphene and bilayer graphene library:   src/raman_materials.json 	modifed version number:   src/setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RamanPL_2D',
-    version='0.1.0',
+    version='0.1.5',
     packages=find_packages(),
     install_requires=[  'numpy>=1.24.4',
                         'matplotlib>=3.5.2',
